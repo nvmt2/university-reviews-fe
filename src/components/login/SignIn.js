cb6e6ff1cdd9b27812d0ab0172be90096b8b4646
ToiNguyen <minhtoi2799@gmail.com>
2020-09-24 09:23:37 +0700

    fixed conflict

--- src/components/login/SignIn.js
+++ src/components/login/SignIn.js
@@ -1,53 +1,46 @@
-<<<<<<< HEAD
 import React, { useRef, useEffect } from "react";
 import "./SignIn.css";
-import "bootstrap/dist/css/bootstrap.css";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import LockOpenIcon from "@material-ui/icons/LockOpen";
 import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import TwitterIcon from "@material-ui/icons/Twitter";
-import { TweenMax, Power2, Elastic} from "gsap";
-=======
-import React from "react";
+import { TweenMax, Power2, Elastic } from "gsap";
 import "./style.css";
->>>>>>> cfadd050cd05bd880172608d1e0281f3fc6434fb
 
 function SignIn() {
   let title = useRef(null);
   let formSignIn = useRef(null);
 
   useEffect(() => {
-    TweenMax.from (
-      title,
-      {
-        duration: 2.5,
-        x: -1000,
-        ease: Elastic.easeOut(1, 0.3)
-      }
-    )
-    TweenMax.from (
-      formSignIn,
-      {
-        duration: 2.5,
-        x: 1000,
-        ease: Elastic.easeOut(1, 0.3)
-      }
-    )
-  },[])
+    TweenMax.from(title, {
+      duration: 2.5,
+      x: -1000,
+      ease: Elastic.easeOut(1, 0.3),
+    });
+    TweenMax.from(formSignIn, {
+      duration: 2.5,
+      x: 1000,
+      ease: Elastic.easeOut(1, 0.3),
+    });
+  }, []);
 
   return (
-<<<<<<< HEAD
     <div className="SignIn">
       <div className="container">
         <div className="row">
-          <h1 className="title-SignIn"
-            ref = { el => {title = el}}
+          <h1
+            className="title-SignIn"
+            ref={(el) => {
+              title = el;
+            }}
           >
             Đăng nhập để cùng chia sẽ cảm nghĩ của bạn nào...
           </h1>
           <form
-            ref = {el => {formSignIn = el}}
+            ref={(el) => {
+              formSignIn = el;
+            }}
           >
             <h2>Đăng nhập</h2>
             <div className="form-SignIn">
@@ -85,16 +78,6 @@ function SignIn() {
               </div>
             </div>
           </form>
-=======
-    <div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-10 border">
-            <h1>Đăng nhập (Sang code)</h1>
-            
-          </div>
-          <div className="col-md-2"></div>
->>>>>>> cfadd050cd05bd880172608d1e0281f3fc6434fb
         </div>
       </div>
     </div>
--- src/components/login/SignUp.js
+++ src/components/login/SignUp.js
@@ -1,53 +1,45 @@
-<<<<<<< HEAD
-import React, {useRef, useEffect} from "react";
-=======
-import React from "react";
-
->>>>>>> cfadd050cd05bd880172608d1e0281f3fc6434fb
+import React, { useRef, useEffect } from "react";
 import "./SignUp.css";
-import "bootstrap/dist/css/bootstrap.css";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import LockOpenIcon from "@material-ui/icons/LockOpen";
 import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
-import { TweenMax, Power2, Bounce} from "gsap";
+import { TweenMax, Power2, Bounce } from "gsap";
 
 function SignUp() {
   let title = useRef(null);
   let formSignUp = useRef(null);
 
   useEffect(() => {
-    TweenMax.from(
-      title,
-      {
-        duration: 1,
-        y: -1000,
-        ease: Bounce
-      }
-    )
-    TweenMax.from(
-      formSignUp,
-      {
-        duration: 1,
-        delay: 1,
-        x: -1250,
-        ease: Power2.in
-      }
-    );
-    
-  }, [])
+    TweenMax.from(title, {
+      duration: 1,
+      y: -1000,
+      ease: Bounce,
+    });
+    TweenMax.from(formSignUp, {
+      duration: 1,
+      delay: 1,
+      x: -1250,
+      ease: Power2.in,
+    });
+  }, []);
 
   return (
     <div className="SignUp">
       <div className="container">
         <div className="row">
-          <h1 className="title-SignUp"
-            ref = {el => {title = el}}
+          <h1
+            className="title-SignUp"
+            ref={(el) => {
+              title = el;
+            }}
           >
             Đăng ký nhanh để trải nghiệm ngay ...
           </h1>
           <form
-            ref={el => {formSignUp = el}}
+            ref={(el) => {
+              formSignUp = el;
+            }}
           >
             <h2>Đăng ký</h2>
             <div className="form-group">
@@ -75,7 +67,9 @@ function SignUp() {
               <button className="done">Xong</button>
               <div className="infor">
                 <p>Theo dõi chúng tôi qua</p>
-                <a href="https://www.facebook.com/messages/t/3555565087788394">https://www.facebook/cs41.04</a>
+                <a href="https://www.facebook.com/messages/t/3555565087788394">
+                  https://www.facebook/cs41.04
+                </a>
               </div>
             </div>
           </form>
