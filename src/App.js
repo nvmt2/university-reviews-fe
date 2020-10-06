fb10092761564340943f558f6a776d7fb761d18c
ToiNguyen <minhtoi2799@gmail.com>
2020-10-06 16:22:49 +0700

    remove css in app.js about text-alight center

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "../node_modules/@fortawesome/fontawesome-free/js/all";
 import { BrowserRouter as Router } from "react-router-dom";
 import RouteWrapper from "./Route";
 import "bootstrap/dist/css/bootstrap.css";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Router>
         <RouteWrapper />
       </Router>
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,32 +1,35 @@
 import React from "react";
 import "./style.css";
 import NavBar from "./nav-link/NavBar";
+// import { } from ""
 
 function Header() {
   return (
     <>
-      <div className="container-fluid border header-page">
+      <div className="container-fluid border background-color-header">
         <div className="row">
           <div className="col-lg-2 border">
             <img src="" alt="logo"></img>
           </div>
           <div className="col-lg-7 border">
-            <ul>
+            <ul className="list-header ">
               <li>
-                <fieldset>
+                <form className="border-red ">
                   <input
-                    className="search"
+                    className="search border-red "
                     type="text"
                     placeholder="Tìm kiếm... "
-                  ></input>
-                </fieldset>
+                  />
+                  <button type="submit">
+                    <i className="fa fa-search" aria-hidden="true"></i>
+                    <span>Nhập tên trường cần tìm</span>
+                  </button>
+                </form>
               </li>
               <li>logo VietNam</li>
-              <li>
-                
-              </li>
+              <li>drop box</li>
+              <li>vi</li>
             </ul>
-          
           </div>
           <div className="col-lg-3 border">
             <NavBar />
