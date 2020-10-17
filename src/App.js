dd84ea33d848ab2592166a7f68cd8c86c2819a4b
ToiNguyen <minhtoi2799@gmail.com>
2020-10-17 11:32:02 +0700

    cofig appolo client

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import "../node_modules/@fortawesome/fontawesome-free/js/all";
 import { BrowserRouter as Router } from "react-router-dom";
 import RouteWrapper from "./Route";
 import "bootstrap/dist/css/bootstrap.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { API_CMS } from "./query/config" ;
+
+
+const client = new ApolloClient({
+  uri: API_CMS,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <div>
+     <ApolloProvider client={client}>
       <Router>
         <RouteWrapper />
       </Router>
+      </ApolloProvider>
     </div>
   );
 }
