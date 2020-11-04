b942436eb5e421cf18f70db0f781f57314916457
= <=>
2020-11-04 12:29:11 +0700

    trang new-post v1

--- src/components/body/new-post/index.js
+++ src/components/body/new-post/index.js
@@ -1,9 +1,28 @@
 import React from "react";
 
+import "./style/main.css";
+
 function index() {
   return (
-    <div>
-      <h1>Đăng bài viết</h1>
+    <div className="news-post">
+      <div className="container col-6 new-post">
+        <h2 className="title-new-post">Đăng bài</h2>
+        <form className="form-new-post">
+          <input
+            type="text"
+            placeholder="Tiêu đề bài viết"
+            className="title-post"
+          />
+          <textarea
+            type="text"
+            placeholder="Chia sẻ ý kiến của bạn"
+            className="content-post"
+          />
+          <input type="text" placeholder="Nhập thẻ tag" className="tag-post" />
+        </form>
+        {/**-------------------button----------- */}
+        <button className="btn-new-post">Đăng bài viết</button>
+      </div>
     </div>
   );
 }
